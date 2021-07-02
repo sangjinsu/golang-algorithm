38dc7ccb02a00fef1c2155c1f85da4e0b70bb68a
jinsuSang <twtz0309@naver.com>
2021-07-02 12:12:10 +0900

    solved

---
+++ AL/boj/bronze3/11022/boj11022.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	var t, a, b int
+
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer func(writer *bufio.Writer) {
+		err := writer.Flush()
+		if err != nil {
+			return
+		}
+	}(writer)
+
+	_, err := fmt.Fscan(reader, &t)
+	if err != nil {
+		return
+	}
+
+	for i := 0; i < t; i++ {
+		_, err2 := fmt.Fscan(reader, &a, &b)
+		if err2 != nil {
+			return
+		}
+
+		_, err3 := writer.WriteString(
+			"Case #" + strconv.Itoa(i+1) + ": " +
+				strconv.Itoa(a) + " + " + strconv.Itoa(b) +
+				" = " + strconv.Itoa(a+b) + "\n")
+		if err3 != nil {
+			return
+		}
+	}
+}
