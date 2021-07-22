f178e6961f45c957753a83697d2d4e2fcc3e78a5
jinsuSang <climbplant39@kakao.com>
2021-07-22 22:50:14 +0900

    implement

--- AL/boj/tag/implement/1110/boj1110.go
+++ AL/boj/tag/implement/1110/boj1110.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	$END$
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+
+	k := n
+	count := 0
+	for true {
+		count++
+		nums := strings.Split(strconv.Itoa(k), "")
+		sum := 0
+		for _, num := range nums {
+			number, _ := strconv.Atoi(num)
+			sum += number
+		}
+		sumArr := strings.Split(strconv.Itoa(sum), "")
+		k, _ = strconv.Atoi(nums[len(nums) - 1] + sumArr[len(sumArr) - 1])
+
+		if k == n {
+			break
+		}
+	}
+
+	fmt.Fprint(writer, count)
 }
