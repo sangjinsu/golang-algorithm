cb8bd9741a8681abbb2fcd049efa6d1796f387a4
jinsuSang <climbplant39@kakao.com>
2021-07-22 22:55:37 +0900

    recursion

--- AL/boj/tag/recursion/10829/boj10829.go
+++ AL/boj/tag/recursion/10829/boj10829.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func changeBinary(num int) string{
+	if num <= 1 {
+		return "1"
+	}
+	return changeBinary(num / 2) + strconv.Itoa(num % 2)
+}
+
 func main() {
-	$END$
+	reader := bufio.NewReader(os.Stdin)
+	writer:= bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+
+	result := changeBinary(n)
+	fmt.Fprint(writer, result)
 }
--- AL/boj/tag/recursion/1769/boj1769.go
+++ AL/boj/tag/recursion/1769/boj1769.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	$END$
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var number string
+	fmt.Fscan(reader, &number)
+	cnt, answer := isMultipleOfThree(number, 0)
+
+	fmt.Fprintln(writer, cnt)
+	fmt.Fprint(writer, answer)
+
+}
+
+func isMultipleOfThree(number string, count int) (int, string) {
+	if len(number) == 1 {
+		n, _ := strconv.Atoi(number)
+		if n%3 == 0 {
+			return count, "YES"
+		} else {
+			return count, "NO"
+		}
+	}
+	count++
+	nums := strings.Split(number, "")
+	total := 0
+	for _, num := range nums {
+		n, _ := strconv.Atoi(num)
+		total += n
+	}
+	t := strconv.Itoa(total)
+	return isMultipleOfThree(t, count)
 }
