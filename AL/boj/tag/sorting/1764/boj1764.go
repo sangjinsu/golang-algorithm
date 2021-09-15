a742a502152ae5a62542bf44ed0b7a56f0fe0d5a
jinsuSang <climbplant39@kakao.com>
2021-09-15 23:08:23 +0900

    hashing

--- AL/boj/tag/sorting/1764/boj1764.go
+++ AL/boj/tag/sorting/1764/boj1764.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
 func main() {
-	$END$
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n, m int
+	fmt.Fscanf(reader, "%d %d\n", &n, &m)
+
+	notHearSee := make(map[string]bool)
+
+	for i := 0; i < n; i++ {
+		var name string
+		fmt.Fscanln(reader, &name)
+		notHearSee[name] = true
+	}
+
+	var result []string
+	for i := 0; i < m; i++ {
+		var name string
+		fmt.Fscanln(reader, &name)
+		_, exist := notHearSee[name]
+		if exist {
+			result = append(result, name)
+		}
+	}
+
+	sort.Strings(result)
+	fmt.Fprintln(writer, len(result))
+	for _, s := range result {
+		fmt.Fprintln(writer, s)
+	}
 }
