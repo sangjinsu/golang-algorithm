7b98a6b1b86e679131f1fa61b993164f25f3aa13
jinsuSang <climbplant39@kakao.com>
2022-03-02 09:44:36 +0900

    20220302

--- AL/boj/tag/dynamic/2225/boj2225.go
+++ AL/boj/tag/dynamic/2225/boj2225.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n, k int
+	fmt.Fscanf(reader, "%d %d\n", &n, &k)
 
 }
